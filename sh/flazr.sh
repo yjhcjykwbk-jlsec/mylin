45928cae3a6279e6b67ebdaf9bc5f4b2bd2213e5
xzg <x@u.(none)>
2013-04-08 16:17:39 +0800

    ..

---
+++ sh/flazr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/x/run/flazr
+sudo sh client.sh rtmp://192.168.3.$1/oflaDemo/hobbit_vp6.flv -load $2
--- sh/ipvs.sh
+++ sh/ipvs.sh
@@ -5,12 +5,12 @@ _echo (){
 }
 _echo 'eth0:0 settings'
 sudo ifconfig eth0:0 down 
-_echo 'sudo ifconfig eth0:0 219.223.195.8 broadcast 219.223.195.8 netmask 255.255.255.0'
-sudo ifconfig eth0:0 219.223.195.8 broadcast 219.223.195.8 netmask 255.255.248.0
-_echo 'sudo route add -host 219.223.195.8 dev eth0:0'
-sudo route add -host 219.223.195.8 dev eth0:0
+_echo 'sudo ifconfig eth0:0 10.7.62.74 broadcast 10.7.62.74 netmask 255.255.255.255'
+sudo ifconfig eth0:0 10.7.62.74 broadcast 10.7.62.74 netmask 255.255.248.0
+_echo 'sudo route add -host 10.7.62.74 dev eth0:0'
+sudo route add -host 10.7.62.74 dev eth0:0
 _echo "ipvs settings"
 sudo ipvsadm -C
-sudo ipvsadm -At 219.223.195.8:80 -s rr
-sudo ipvsadm -at 219.223.195.8:80 -r 219.223.192.135 -g
+sudo ipvsadm -At 10.7.62.74:80 -s rr
+sudo ipvsadm -at 10.7.62.74:80 -r 219.223.192.135:80 -g
 sudo ipvsadm -L -n
