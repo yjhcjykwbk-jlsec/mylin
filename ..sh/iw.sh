13ee1a9aae8157e4cd951a09ab337481c0c8c874
xzg <x@u.ubuntu-domain>
2013-06-07 19:27:06 +0800

    add wifi

---
+++ ..sh/iw.sh
@@ -0,0 +1,6 @@
+sudo ifconfig wlan0 up
+echo "sudo ifconfig wlan0 up"
+sudo iwconfig wlan0 essid UTSZ channel 01
+echo "sudo iwconfig wlan0 essid UTSZ channel 01"
+sudo dhclient wlan0
+echo "sudo dhclient wlan0"
