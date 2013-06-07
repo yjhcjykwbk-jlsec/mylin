06773fbf5173c3a79403c1e6c2743e0e87f8b3b4
xzg <x@u.ubuntu-domain>
2013-06-08 00:42:45 +0800

    change wifi.sh

--- ..sh/iw.sh
+++ ..sh/iw.sh
@@ -1,6 +1,22 @@
-sudo ifconfig wlan0 up
+##first make sure your block is off for wifi
+#sudo rfkill unblock wifi  #解锁wifi
+#sudo rfkill list all      #查看wifi锁状况
+#{
+	#0: ideapad_wlan: Wireless LAN
+	#	Soft blocked: no
+	#	Hard blocked: no
+	#1: phy0: Wireless LAN
+	#	Soft blocked: no
+	#	Hard blocked: yes
+#}
+
+#echo "sudo ifconfig wlan0 down"
+#sudo ifconfig wlan0 down
+
 echo "sudo ifconfig wlan0 up"
-sudo iwconfig wlan0 essid UTSZ channel 01
-echo "sudo iwconfig wlan0 essid UTSZ channel 01"
-sudo dhclient wlan0
+sudo ifconfig wlan0 up
+echo "sudo iwconfig wlan0 essid UTSZ channel $1"
+sudo iwconfig wlan0 essid UTSZ channel $1
+#RTNETLINK answers: Operation not possible due to RF-kill
 echo "sudo dhclient wlan0"
+sudo dhclient wlan0
