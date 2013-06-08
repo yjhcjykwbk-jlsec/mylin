6176adfd910e574c683ceade235806e50eca1897
xzg <x@u.ubuntu-domain>
2013-06-09 04:03:40 +0800

    ..

--- syn.sh
+++ syn.sh
@@ -2,3 +2,5 @@ cp ~/.vimrc ./
 cp ~/.bash_aliases ./
 cp ~/..sh ./ -r 
 cp /etc/apache2/sites-available/default etc.apache2.sites-available.default
+cp /etc/rc.local etc.rc.local
+git add ..sh .bash_aliases .vimrc etc.* syn.sh
