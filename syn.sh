9ee7dfd5d8f489280f359283402f672f57a47548
xzg <x@u.(none)>
2013-06-10 01:41:39 +0800

    ..

--- syn.sh
+++ syn.sh
@@ -1,14 +1,14 @@
-myecho (){
-  echo -e "\033 $1 \033[0m";
+myecho(){
+  echo -e "\033".$1."\033[0m"
 }
-myecho "##now synchronizing files to git..."
+myecho "now synchronizing files to git..."
 cp ~/.vimrc ./
 cp ~/.bash_aliases ./
 cp ~/..sh ./ -r 
 cp /etc/apache2/sites-available/default etc.apache2.sites-available.default
 cp /etc/rc.local etc.rc.local
 git add ..sh .bash_aliases .vimrc etc.* syn.sh
-myecho "##now commit"
+myecho "now commit"
 git commit -m "$1"
-myecho "##now push to remote server"
+myecho "now push to remote server"
 git push origin master
