94742b2be95d2491c004614b94c9b8354ead6736
xzg <x@u.(none)>
2013-06-10 01:36:20 +0800

    ..

--- syn.sh
+++ syn.sh
@@ -1,6 +1,11 @@
+echo "now synchronizing files to git..."
 cp ~/.vimrc ./
 cp ~/.bash_aliases ./
 cp ~/..sh ./ -r 
 cp /etc/apache2/sites-available/default etc.apache2.sites-available.default
 cp /etc/rc.local etc.rc.local
 git add ..sh .bash_aliases .vimrc etc.* syn.sh
+echo "now commit"
+git commit -m "$1"
+echo "now push to remote server"
+git push origin master
