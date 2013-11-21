b4cbcfc1fcc4d894aad358bcde5268a8262d656f
xzg <x@ubuntu.(none)>
2013-11-21 12:57:57 +0800

    ..

--- syn.sh
+++ syn.sh
@@ -1,10 +1,9 @@
 echo '【now synchronizing files to git...】'
-cp ~/.vimrc ./
 cp ~/.bash_aliases ./
-cp ~/..sh ./ -r 
+cp ~/.sh ./ -r 
 cp /etc/apache2/sites-available/default etc.apache2.sites-available.default
 cp /etc/rc.local etc.rc.local
-git add ..sh .bash_aliases .vimrc etc.* syn.sh
+git add -A
 echo "【now commit】"
 git commit -m "$1"
 echo "【now push to remote server】"
