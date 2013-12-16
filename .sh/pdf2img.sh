bf357b5129e4d80e37bc5c91e73452412253ca06
stevenberge <zgxu2008@aliyun.com>
2013-12-17 00:13:45 +0800

    auto from syn.sh

--- .sh/pdf2img.sh
+++ .sh/pdf2img.sh
@@ -2,10 +2,19 @@ echo "pdf2img [xx.pdf] [pagenum]"
 i=1
 while [ $i -le $2 ] 
 do 
-  echo "pdf2svg $1 $i.svg $i" && i=`expr $i + 1`
+  echo "pdf2svg $1 $i.svg $i"
+  i=`expr $i + 1`
 done;
+
 [ $2 != "" ] || exit
 read -p "trans now, Y/N ?" choice
-echo $choice
 [ $choice != "Y" ] && exit
-i=1;while [ $i -le $2 ] ;do pdf2svg $1 $i.svg $i && i=`expr $i + 1`; done;
+
+i=1
+while [ $i -le $2 ] 
+do 
+  pdf2svg $1 $i.svg $i 
+  convert $i.svg $i.png
+  rm $i.svg
+  i=`expr $i + 1`
+done
--- syn.sh
+++ syn.sh
@@ -5,6 +5,6 @@ cp /etc/apache2/sites-available/default etc.apache2.sites-available.default
 cp /etc/rc.local etc.rc.local
 git add -A
 echo "【now commit】"
-git commit -m "$1"
+git commit -m "auto from syn.sh"
 echo "【now push to remote server】"
 git push origin master
