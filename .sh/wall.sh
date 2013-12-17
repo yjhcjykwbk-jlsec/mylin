a91c468ab58056f8d0f6b98fd9be6affb3eef33a
stevenberge <zgxu2008@aliyun.com>
2013-12-17 13:04:01 +0800

    auto from syn.sh

--- .sh/wall.sh
+++ .sh/wall.sh
@@ -1,13 +1,14 @@
-cd ~/pic
 [ -z $1 ] && exit
+mode="stretch"
+[ -z $2 ] || mode=$2
 while [ true ]
 do
-  for file in `ls .`
+  for file in `ls --file-type *jpg`
   do
     if [ -f $file ]
     then
       sleep $1
-      pcmanfm  -w /home/x/pic/$file
+      pcmanfm  -w `pwd`/$file --wallpaper-mode=$mode
     fi
   done
 done
